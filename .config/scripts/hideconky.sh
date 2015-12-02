882e90812f2cd776bbd642e319907a395711b773
Timothée Poisot <tim@poisotlab.io>
2015-12-02 15:56:33 -0500

    Conky / bspwm integration

--- .config/scripts/hideconky.sh
+++ .config/scripts/hideconky.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 
+connectedscreens=`xrandr | grep " connected" | wc -l`
+
+if test $connectedscreens = 1
+then
+   activescreen=`xrandr | grep " connected" | cut -d' ' -f1`
+else
+   activescreen=`xrandr | grep primary | cut -d' ' -f1`
+fi;
+
+
 if xdotool search --onlyvisible --classname 'Conky' windowunmap;
 then
-   bspc config -m DP1 right_padding 0;
+   bspc config -m $activescreen right_padding 0;
 else
-   bspc config -m DP1 right_padding 148;
+   bspc config -m $activescreen right_padding 200;
    xdotool search --classname 'Conky' windowmap 
 fi;
