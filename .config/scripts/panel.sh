7387ba2fdf8bba79996443e1b010a2a9f79caf8a
Timothée Poisot <tim@poisotlab.io>
2016-03-23 19:52:46 -0400

    new bspwm version

--- .config/scripts/panel.sh
+++ .config/scripts/panel.sh
@@ -10,26 +10,18 @@ else
    activescreen=`xrandr | grep "eDP1" | cut -d' ' -f1`
 fi;
 
-background=`xrdb -q | grep back | cut -f2 -d#`
-foreground=`xrdb -q | grep fore | cut -f2 -d#`
-
-# If compton is running, transparent bar
-if test $(pidof compton | wc -l) = 1
-then
-   background="00$background"
-fi
-background="1d1f21"
-foreground="FFFFFF" 
+background=$(grep "background\s" ~/.config/termite/config | cut -d= -f2 | tr -d ' ' | tr -d '#')
+foreground=$(grep "foreground\s" ~/.config/termite/config | cut -d= -f2 | tr -d ' ' | tr -d '#')
 
 if test $lemonbarrunning = 1
 then
    bspc config -m $activescreen bottom_padding 0
    bspc config -m $activescreen top_padding 0
    killall lemonbar
+   for pid in $(ps -ef | grep "lemonbar.sh" | awk '{print $2}'); do kill -9 $pid; done
 else
    bspc config -m $activescreen top_padding 27
    bspc config -m $activescreen bottom_padding 0
    ~/.config/scripts/lemonbar.sh | lemonbar -B"#$background" -F"#$foreground" -g1920x27+0+0 -u1 -f IosevkaNL-10 -f FontAwesome-11 -o 0 -d & 
-   #~/.config/scripts/lemonbar.sh | lemonbar -B"#$background" -F"#$foreground" -g1920x19 -u1 -f IosevkaNL-10 -o 0 -b -d & 
 fi;
 
